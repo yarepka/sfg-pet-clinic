f9720a69bc39f08f9e68ccf018aac4bdacaced2f
yarepka <yarepka01@gmail.com>
2018-11-07 22:37:14 +0300

    Added Specialties Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+import guru.springframework.sfgpetclinic.services.SpecialtiesService;
+
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+}
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
@@ -10,12 +11,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+
 
     @Autowired
     public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
@@ -41,12 +45,33 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Vladimir");
         owner1.setLastName("Ulyanov");
+        owner1.setAddress("144 Green");
+        owner1.setCity("Pensacola");
+        owner1.setTelephone("+12838444392");
+
+        Pet vladimirsPet = new Pet();
+        vladimirsPet.setName("Quake");
+        vladimirsPet.setPetType(savedDogPetType);
+        vladimirsPet.setOwner(owner1);
+        vladimirsPet.setBirthDate(LocalDate.now());
+        owner1.getPets().add(vladimirsPet);
+
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Ekaterina");
         owner2.setLastName("Romanova");
+        owner2.setAddress("Lenina");
+        owner2.setCity("Chelyabinsk");
+        owner2.setTelephone("+79113453453");
         ownerService.save(owner2);
+
+        Pet katesPet = new Pet();
+        katesPet.setName("Rainbow");
+        katesPet.setPetType(savedCatPetType);
+        katesPet.setOwner(owner2);
+        katesPet.setBirthDate(LocalDate.now());
+        owner2.getPets().add(katesPet);
 
         System.out.println("Loaded Owners....");
 
