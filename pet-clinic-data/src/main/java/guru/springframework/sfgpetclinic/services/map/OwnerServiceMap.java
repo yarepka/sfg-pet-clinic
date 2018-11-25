f2912cee3f937d61c63a848317944551aeecd996
yarepka <yarepka01@gmail.com>
2018-11-25 14:35:12 +0300

    added tests. Closes #49

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -5,6 +5,7 @@ import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.PetService;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,7 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     private final PetTypeService petTypeService;
     private final PetService petService;
 
+    @Autowired
     public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
         this.petTypeService = petTypeService;
         this.petService = petService;
@@ -25,18 +27,18 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public Owner findByLastName(String lastName) {
         return map.entrySet().stream()
-                .filter(e -> e.getValue().getFirstName().equals(lastName))
+                .filter(e -> e.getValue().getLastName().equalsIgnoreCase(lastName))
                 .map(Map.Entry::getValue)
                 .findFirst()
                 .orElse(null);
     }
 
     @Override
-    public Owner save(Owner object) {
+    public Owner save(Owner owner) {
 
-        if(object != null) {
-            if(object.getPets() != null) {
-                object.getPets().forEach(pet -> {
+        if(owner != null) {
+            if(owner.getPets() != null) {
+                owner.getPets().forEach(pet -> {
                     if(pet.getPetType() != null) {
                         pet.setPetType(petTypeService.save(pet.getPetType()));
                     } else {
@@ -50,7 +52,7 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
                 });
             }
 
-            return super.save(object);
+            return super.save(owner);
 
         } else {
             return null;
---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMapTest.java
@@ -0,0 +1,88 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerServiceMapTest {
+
+    OwnerServiceMap ownerServiceMap;
+
+    final Long ownerId = 1L;
+    final String lastName = "Smith";
+
+    @BeforeEach
+    void setUp() {
+
+        ownerServiceMap = new OwnerServiceMap(new PetTypeServiceMap(), new PetServiceMap());
+
+        ownerServiceMap.save(Owner.builder().id(ownerId).lastName(lastName).build());
+    }
+
+    @Test
+    void findByLastName() {
+        Owner owner = ownerServiceMap.findByLastName(lastName);
+
+        assertNotNull(owner);
+
+        assertEquals(ownerId, owner.getId());
+    }
+
+    @Test
+    void findByLastNameNotFound() {
+        Owner owner = ownerServiceMap.findByLastName("foo");
+
+        assertNull(owner);
+    }
+
+    @Test
+    void saveExistingId() {
+        Long id = 2L;
+
+        Owner owner = Owner.builder().id(id).build();
+
+        Owner savedOwner = ownerServiceMap.save(owner);
+
+        assertEquals(id, savedOwner.getId());
+    }
+
+    @Test
+    void saveNoId() {
+        Owner savedOwner = ownerServiceMap.save(Owner.builder().build());
+
+        assertNotNull(savedOwner);
+        assertNotNull(savedOwner.getId());
+    }
+
+    @Test
+    void findAll() {
+        Set<Owner> ownerSet = ownerServiceMap.findAll();
+
+        assertEquals(1, ownerSet.size());
+    }
+
+    @Test
+    void findById() {
+        Owner owner = ownerServiceMap.findById(ownerId);
+
+        assertEquals(ownerId, owner.getId());
+    }
+
+    @Test
+    void delete() {
+        ownerServiceMap.delete(ownerServiceMap.findById(ownerId));
+
+        assertEquals(0, ownerServiceMap.findAll().size());
+    }
+
+    @Test
+    void deleteById() {
+        ownerServiceMap.deleteById(ownerId);
+
+        assertEquals(0, ownerServiceMap.findAll().size());
+    }
+}
